644809847b1a91d9f35987b30cc390f1b4aa0a22
FabulousStarfish <71825170+FabulousStarfish@users.noreply.github.com>
2021-05-22 11:15:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+status="";
 function setup(){
     canvas=createCanvas(500,350);
     canvas.position(500,275);
+
+    video=createCapture(VIDEO);
+    video.size(500,350);
+    video.hide();    
+}
+function start(){
+    cocossd=ml5.objectDetector('cocossd',modalLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function modalLoaded(){
+    console.log("MODAL LOADED !");
+    status=true;
+}
+function draw(){
+    image(video,0,0,500,350)
 }
